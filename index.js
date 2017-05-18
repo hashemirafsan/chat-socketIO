ad187702070345ac5bde52a7b54ca634a83c1570
Hashemi Rafsan <rafsanhashemi@gmail.com>
2017-05-18 18:12:26 +0600

    Chat Upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req,res) => {
+	res.sendFile('index.html');
+})
+
+io.on('connection', (socket) => {
+
+	socket.on('disconnect', () => {
+    	console.log('user disconnected');
+  	});
+
+  	socket.on('chat', (msg,id) => {
+  		io.emit('chat', msg,id);
+  	})
+
+  	socket.on('typing', (msg,id) => {
+  		io.emit('typing', msg,id);
+  	})
+}) 
+
+http.listen(process.env.port || 3000, () => {
+	console.log('app start')
+})
